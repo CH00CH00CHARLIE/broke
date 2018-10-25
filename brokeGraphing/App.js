c5c5444fd52b1c542f86a78071feeb91068bd2b3
HarshitBagla <harshit.bagla@gmail.com>
2018-10-25 13:11:25 -0500

    Samples of all the graphs we can work with.

--- brokeGraphing/App.js
+++ brokeGraphing/App.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-import { AreaChart, Grid } from 'react-native-svg-charts'
+import { AreaChart, Grid, BarChart, ProgressCircle, LineChart, PieChart } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
 
-import { ProgressCircle }  from 'react-native-svg-charts'
- 
-class ProgressCircleExample extends React.PureComponent {
- 
+class PieChartExample extends React.PureComponent {
+
     render() {
- 
+
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+        const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
+
+        const pieData = data
+            .filter(value => value > 0)
+            .map((value, index) => ({
+                value,
+                svg: {
+                    fill: randomColor(),
+                    onPress: () => console.log('press', index),
+                },
+                key: `pie-${index}`,
+            }))
+
+        return (
+            <PieChart
+                style={ { height: 200 } }
+                data={ pieData }
+            />
+        )
+    }
+}
+
+class LineChartExample extends React.PureComponent {
+
+    render() {
+
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+        return (
+            <LineChart
+                style={{ height: 200 }}
+                data={ data }
+                svg={{ stroke: 'rgb(134, 65, 244)' }}
+                contentInset={{ top: 20, bottom: 20 }}
+            >
+                <Grid/>
+            </LineChart>
+        )
+    }
+
+}
+
+class ProgressCircleExample extends React.PureComponent {
+
+    render() {
+
         return (
             <ProgressCircle
                 style={ { height: 200 } }
@@ -18,16 +64,36 @@ class ProgressCircleExample extends React.PureComponent {
             />
         )
     }
- 
+
 }
- 
- 
-class AreaChartExample extends React.PureComponent {
- 
+
+class BarChartExample extends React.PureComponent {
+
     render() {
- 
+
+        const fill = 'rgb(134, 65, 244)'
+        const data   = [ 50, 10, 40, 95, -4, -24, null, 85, undefined, 0, 35, 53, -53, 24, 50, -20, -80 ]
+
+        return (
+            <BarChart
+                style={{ height: 200 }}
+                data={ data }
+                svg={{ fill }}
+                contentInset={{ top: 30, bottom: 30 }}
+            >
+                <Grid/>
+            </BarChart>
+        )
+    }
+
+}
+
+class AreaChartExample extends React.PureComponent {
+
+    render() {
+
         const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
- 
+
         return (
             <AreaChart
                 style={{ height: 200 }}
@@ -55,17 +121,17 @@ export default class AlignItemsBasics extends Component {
         alignItems: 'stretch',
       }}>
         <AreaChartExample/>
-        <View style={{height: 50, backgroundColor: 'skyblue'}} />
-
+        <BarChartExample/>
         <ProgressCircleExample/>
-        <View style={{height: 100, backgroundColor: 'steelblue'}} />
+        <LineChartExample/>
+        <PieChartExample/>
       </View>
     );
   }
 }
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics);
+//AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics);
 
 // import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
