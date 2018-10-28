d0fb318e9f0a7a8ca715ee280370277a8d629bc5
Chirag Jain <masterchi20@icloud.com>
2018-10-28 12:05:38 -0500

    Devmode is working. No UI is provided since this is a backend file. Still trying to figure out how to send user data.

--- devMode/index.js
+++ devMode/index.js
@@ -3,10 +3,12 @@
 var util = require('util');
 
 var envvar = require('envvar');
+var express = require('express');
 var plaid = require('plaid');
 var bodyParser = require('body-parser');
 var moment = require('moment');
 
+var APP_PORT = envvar.number('APP_PORT', 8000);
 var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID', '5bb63c2cf17fb3001118dd22');
 var PLAID_SECRET = envvar.string('PLAID_SECRET', '25988c12c60164e32d73351e562718');
 var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY', 'ac25fbf77a17846194a680a183096a');
@@ -24,16 +26,150 @@ var client = new plaid.Client(
   {version: '2018-05-22'}
 );
 
-client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.post('/get_access_token', function(request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  console.log(PUBLIC_TOKEN);
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
     if (error != null) {
       console.log(error);
-      return null;
+      return response.json({
+        error: error,
+      });
     }
     ACCESS_TOKEN = tokenResponse.access_token;
     ITEM_ID = tokenResponse.item_id;
-    console.log(tokenResponse);
+    prettyPrintResponse(tokenResponse);
+    response.json({
+      access_token: ACCESS_TOKEN,
+      item_id: ITEM_ID,
+      error: null,
+    });
+  });
 });
 
+// Retrieve Transactions for an Item
+// https://plaid.com/docs/#transactions
+app.get('/transactions', function(request, response, next) {
+  // Pull transactions for the Item for the last 30 days
+  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error
+      });
+    } else {
+      prettyPrintResponse(transactionsResponse);
+      response.json({error: null, transactions: transactionsResponse});
+    }
+  });
+});
+
+// Retrieve Identity for an Item
+// https://plaid.com/docs/#identity
+app.get('/identity', function(request, response, next) {
+  client.getIdentity(ACCESS_TOKEN, function(error, identityResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    prettyPrintResponse(identityResponse);
+    response.json({error: null, identity: identityResponse});
+  });
+});
+
+// Retrieve real-time Balances for each of an Item's accounts
+// https://plaid.com/docs/#balance
+app.get('/balance', function(request, response, next) {
+  client.getBalance(ACCESS_TOKEN, function(error, balanceResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    prettyPrintResponse(balanceResponse);
+    response.json({error: null, balance: balanceResponse});
+  });
+});
+
+// Retrieve an Item's accounts
+// https://plaid.com/docs/#accounts
+app.get('/accounts', function(request, response, next) {
+  client.getAccounts(ACCESS_TOKEN, function(error, accountsResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    prettyPrintResponse(accountsResponse);
+    response.json({error: null, accounts: accountsResponse});
+  });
+});
+
+// Retrieve ACH or ETF Auth data for an Item's accounts
+// https://plaid.com/docs/#auth
+app.get('/auth', function(request, response, next) {
+  client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    prettyPrintResponse(authResponse);
+    response.json({error: null, auth: authResponse});
+  });
+});
+
+// Retrieve information about an Item
+// https://plaid.com/docs/#retrieve-item
+app.get('/item', function(request, response, next) {
+  // Pull the Item - this includes information about available products,
+  // billed products, webhook information, and more.
+  client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error
+      });
+    }
+    // Also pull information about the institution
+    client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
+      if (err != null) {
+        var msg = 'Unable to pull institution information from the Plaid API.';
+        console.log(msg + '\n' + JSON.stringify(error));
+        return response.json({
+          error: msg
+        });
+      } else {
+        prettyPrintResponse(itemResponse);
+        response.json({
+          item: itemResponse.item,
+          institution: instRes.institution,
+        });
+      }
+    });
+  });
+});
+
+var server = app.listen(APP_PORT, function() {
+  console.log('plaid-quickstart server listening on port ' + APP_PORT);
+});
 
 var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
 var endDate = moment().format('YYYY-MM-DD');
